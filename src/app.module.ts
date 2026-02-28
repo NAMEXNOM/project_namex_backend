12388d176cd5cf85d9970a64fae069b4ef94f95c
Ramon <ramon.anchondo@gmail.com>
2026-02-28 10:50:08 -0600

    preconfiguracion de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+
+  // add db  02-28-26 de nesjs techniques-database
+  TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: '123456',
+      database: 'portaldb',
+      entities: [],
+      synchronize: true,
+    }),
+// fin  add db  02-28-26 de nesjs techniques-database
+
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
