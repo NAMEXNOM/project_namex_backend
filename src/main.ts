0bf0609f72301aca910deb4ea4c3004b09d677ec
Ramon <ramon.anchondo@gmail.com>
2026-02-28 10:07:53 -0600

    git

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger  se copi de nesjs.doc  openapi  02-28-26  AHR SWAGGER
+
+ const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
+  // FIN DE Swagger
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
