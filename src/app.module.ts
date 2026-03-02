492016acbd987a0564e92eed2bfd40de9d44f156
Ramon <ramon.anchondo@gmail.com>
2026-03-02 16:15:16 -0600

    se guardaron cambios de dbconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
-      username: 'root',
+      port: 5433,
+      username: 'postgres',
       password: '123456',
       database: 'portaldb',
       entities: [],
