8f4d63fe4e19ea289dacc8f079ac3f93447551d7
Ramon <ramon.anchondo@gmail.com>
2026-04-02 15:28:29 -0600

    regeneracion de tablas

--- src/database/migrations/1775161858187-create_tablas.ts
+++ src/database/migrations/1775161858187-create_tablas.ts
@@ -1,11 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreatePermissionRoleUsersTable1774989037214 implements MigrationInterface {
-    name = 'CreatePermissionRoleUsersTable1774989037214'
+export class CreateTablas1775161858187 implements MigrationInterface {
+    name = 'CreateTablas1775161858187'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "permission" ("id" SERIAL NOT NULL, "action" character varying, "subject" character varying, "label" character varying, CONSTRAINT "PK_3b8b97af9d9d8807e41e6f48362" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "roles" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying, CONSTRAINT "UQ_648e3f5447f725579d7d4ffdfb7" UNIQUE ("name"), CONSTRAINT "PK_c1433d71a4838793a49dcad46ab" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "user" ("userId" uuid NOT NULL DEFAULT uuid_generate_v4(), "userRFC" character varying NOT NULL, "empNumber" character varying NOT NULL, "name" character varying NOT NULL, "firstLastName" character varying NOT NULL, "secondLastName" character varying NOT NULL, "email" character varying NOT NULL, "hireDate" TIMESTAMP NOT NULL, "termDate" TIMESTAMP, "status" character varying NOT NULL, "shiftType" character varying NOT NULL, "jobRole" character varying NOT NULL, "firstTimeLoad" boolean NOT NULL DEFAULT true, "password" character varying NOT NULL, "empPriv" character varying NOT NULL DEFAULT 'usuario', "vacationBalance" integer NOT NULL DEFAULT '0', "balanceDateTime" TIMESTAMP, CONSTRAINT "UQ_8be01cc2a2dd7cdbb6fb1799980" UNIQUE ("userRFC"), CONSTRAINT "UQ_2b4c50bca5f8f00c04ad6d84138" UNIQUE ("empNumber"), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_d72ea127f30e21753c9e229891e" PRIMARY KEY ("userId"))`);
+        await queryRunner.query(`CREATE TABLE "configuration" ("config_id" integer NOT NULL, "RH1_name" character varying, "RH1_email" character varying, "RH2_name" character varying, "RH2_email" character varying, "RH3_name" character varying, "RH3_email" character varying, "RH4_name" character varying, "RH4_email" character varying, "RH5_name" character varying, "RH5_email" character varying, "RH6_name" character varying, "RH6_email" character varying, "nameCo" character varying DEFAULT '', "rfcCo" character varying DEFAULT '', "urlCo" character varying DEFAULT '', "portNumber" character varying DEFAULT '', "token" character varying DEFAULT '', CONSTRAINT "UQ_1bfecb9c4cc4746e6dd2660dcdf" UNIQUE ("RH1_name"), CONSTRAINT "UQ_55976348608c82717bd6b3ff55a" UNIQUE ("RH1_email"), CONSTRAINT "UQ_7a4adf8cc2623fa4a5a14e57e12" UNIQUE ("RH2_name"), CONSTRAINT "UQ_8d49fe0e16370d4e7ce531f576a" UNIQUE ("RH2_email"), CONSTRAINT "UQ_47af08902e6a8a77b13ac2a9cd9" UNIQUE ("RH3_name"), CONSTRAINT "UQ_0f5b6159e4b6b42685fa09eba57" UNIQUE ("RH3_email"), CONSTRAINT "UQ_1d1cd343878a941ef2246961e56" UNIQUE ("RH4_name"), CONSTRAINT "UQ_a8e3ecacd670195d432c82b8426" UNIQUE ("RH4_email"), CONSTRAINT "UQ_e3b0a015c9f16d8b28e23613ff1" UNIQUE ("RH5_name"), CONSTRAINT "UQ_ea54ac612c6afc50a1c8ccbff63" UNIQUE ("RH5_email"), CONSTRAINT "UQ_a58b91ae7ef2dbb06954abe2f34" UNIQUE ("RH6_name"), CONSTRAINT "UQ_9aeea13ce70ba7c85f87c54b925" UNIQUE ("RH6_email"), CONSTRAINT "PK_bfecc86452256847d8bbb4c1b3c" PRIMARY KEY ("config_id"))`);
         await queryRunner.query(`CREATE TABLE "role_permissions" ("role_id" integer NOT NULL, "permission_id" integer NOT NULL, CONSTRAINT "PK_25d24010f53bb80b78e412c9656" PRIMARY KEY ("role_id", "permission_id"))`);
         await queryRunner.query(`CREATE INDEX "IDX_178199805b901ccd220ab7740e" ON "role_permissions" ("role_id") `);
         await queryRunner.query(`CREATE INDEX "IDX_17022daf3f885f7d35423e9971" ON "role_permissions" ("permission_id") `);
@@ -29,6 +31,8 @@ export class CreatePermissionRoleUsersTable1774989037214 implements MigrationInt
         await queryRunner.query(`DROP INDEX "public"."IDX_17022daf3f885f7d35423e9971"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_178199805b901ccd220ab7740e"`);
         await queryRunner.query(`DROP TABLE "role_permissions"`);
+        await queryRunner.query(`DROP TABLE "configuration"`);
+        await queryRunner.query(`DROP TABLE "user"`);
         await queryRunner.query(`DROP TABLE "roles"`);
         await queryRunner.query(`DROP TABLE "permission"`);
     }
